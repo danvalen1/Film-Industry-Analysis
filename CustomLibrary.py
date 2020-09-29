9238780c6a3cc111529f0994544d1adbf4fc7334
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2020-09-29 10:04:11 -0400

    changes to function library

--- CustomLibrary.py
+++ CustomLibrary.py
@@ -1,2 +1,32 @@
-def SetUpLibraries():
-	print('hello world!')
+
+def indicator_str_parser(dataframe, parsed_column_str, list_of_strs):
+    
+    # If column full of strings has no string to be parsed, set value to 'none'
+    dataframe[dataframe[parsed_column_str].isnull()] = "none"
+    
+    # Create indicator columns for columns with no string to be parsed
+    dataframe[parsed_column_str + '_not_parsed_id'] = [1 if x == "none"
+                                                       else 0 
+                                                       for x in dataframe[parsed_column_str]]
+    
+    
+    
+    # starts list of created series to be used as arguments
+    list_of_series = []
+    
+    # Loop over elements in list
+    for genre in list_of_strs:
+        
+        # Make a new indicator column from the parsed column adn the element to be searched for
+        dataframe[parsed_column_str + '_' + genre + '_id'] = [1 if genre in x 
+                                                            else 0 
+                                                            for x in dataframe[parsed_column_str]]
+        
+        # Include new column in list to be fed into function
+        list_of_series.append(dataframe[parsed_column_str + '_' + genre + '_id'])
+        
+    # Unpack list_of_series to be fed as arguments into zip function for unique tuples
+    dataframe['genre_tuple'] = list(zip(*list_of_series))
+    
+    # return dataframe for quick view/analysis
+    return dataframe
