d82de92bd8821ffd6f7b9eaa31d07e2a451307b4
Lauren Phipps <lhphipps@gmail.com>
2020-10-01 17:17:16 -0400

    add functions

--- CustomLibrary.py
+++ CustomLibrary.py
@@ -3,7 +3,7 @@ def import_libs():
     import pandas as pd
     import matplotlib.pyplot as plt
     import seaborn as sns
-    
+    import matplotlib.ticker as mtick
     
 
 # Setting standard style
@@ -219,3 +219,194 @@ def HB_Genres_Graph(DataFrame):
 
     
     
+
+def budget_ROI_scatter (DataFrame):
+
+    #Create a figure
+    fig, ax = plt.subplots(figsize = (25, 20))
+    
+    #Create text and color settings for all plots
+    sns.set(font_scale = 3, style = 'whitegrid')
+    
+    #Create a scatterplot with Total Costs on the x-axis and ROI on the y-axis
+    sns.scatterplot(data = df, x = 'total_costs', y = 'ROI', ax = ax)
+    
+    #Create axis limits on the y-axis to better frame the data
+    plt.ylim(-200, 1500)
+    
+    #Create axis labels and a title and shift the x-axis label down
+    plt.xlabel('Movie Budget')
+    plt.ylabel('Return on Investment')
+    plt.title('Relationship Between Movie Budgets and Return on Investment')
+    ax.xaxis.labelpad = 20
+    
+    #Format axis labels to contain percent and dollar signs and rotate the x-axis labels
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    fmt = '${x:,.0f}'
+    tick = mtick.StrMethodFormatter(fmt)
+    ax.xaxis.set_major_formatter(tick)
+    plt.xticks(rotation=65)
+
+    #Show plot
+    return plt.show()
+
+
+def LB_ROI_scatter(DataFrame):
+
+    #Create a figure
+    fig, ax = plt.subplots(figsize = (20, 12))
+    
+    #Create a scatterplot with for Low-Budget films with Total Costs on the x-axis and ROI on the y-axis
+    sns.scatterplot(data = df[df['budget_category'] == 'low'], 
+                    x = 'total_costs', 
+                    y = 'ROI', 
+                    ax = ax, 
+                    color = 'skyblue')
+    
+    #Create axis limits on the y-axis to better frame the data
+    plt.ylim(-500, 3500)
+    
+    #Create axis labels and a title
+    plt.xlabel('Movie Budget')
+    plt.ylabel('Return on Investment')
+    plt.title('Low Budget Films Return on Investment (in %)')
+    
+    #Format axis labels to contain percent and dollar signs and rotate the x-axis labels
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    fmt = '${x:,.0f}'
+    tick = mtick.StrMethodFormatter(fmt)
+    ax.xaxis.set_major_formatter(tick)
+    plt.xticks(rotation=65)
+    
+    #Show plot
+    return plt.show()
+
+
+
+def MB_ROI_scatter (DataFrame):
+
+    #Create a figure
+    fig, ax = plt.subplots(figsize = (20, 12))
+    
+    #Create a scatterplot with for Mid-Budget films with Total Costs on the x-axis and ROI on the y-axis
+    sns.scatterplot(data = df[df['budget_category'] == 'mid'], x = 'total_costs', y = 'ROI', ax = ax, color = 'steelblue')
+
+    #Create axis labels and a title
+    plt.xlabel('Movie Budget')
+    plt.ylabel('Return on Investment')
+    plt.title('Mid Budget Films Return on Investment (in %)')
+    
+    #Format axis labels to contain percent and dollar signs and rotate the x-axis labels
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    fmt = '${x:,.0f}'
+    tick = mtick.StrMethodFormatter(fmt)
+    ax.xaxis.set_major_formatter(tick)
+    plt.xticks(rotation=65)
+    
+    #Show plot
+    return plt.show()
+
+
+
+def HB_ROI_scatter (DataFrame):
+    
+    #Create a figure
+    fig, ax = plt.subplots(figsize = (20, 12))
+    
+    #Create a scatterplot with for High-Budget films with Total Costs on the x-axis and ROI on the y-axis
+    sns.scatterplot(data = df[df['budget_category'] == 'high'], x = 'total_costs', y = 'ROI', ax = ax, color = 'royalblue')
+    
+    #Create axis labels and a title and shift x-axis label down
+    plt.xlabel('Movie Budget')
+    plt.ylabel('Return on Investment')
+    plt.title('High Budget Films Return on Investment (in %)')
+    ax.xaxis.labelpad = 20
+    
+    #Format axis labels to contain percent and dollar signs and rotate the x-axis labels
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    fmt = '${x:,.0f}'
+    tick = mtick.StrMethodFormatter(fmt)
+    ax.xaxis.set_major_formatter(tick)
+    plt.xticks(rotation=65)
+    
+    #Show plot
+    return plt.show()
+
+
+def ROI_budget_boxplot(DataFrame):
+
+    #Create a figure
+    fig, ax = plt.subplots(figsize = (20,12))
+    
+    #Create a boxplot for ROI distribution, grouped by budget category. Do now show outliers, increase the line thickness for all parts of the boxplot, and set the colors for the lines. Change the budget tier order to low, mid, and high. 
+    DataFrame.boxplot(column = ['ROI'], by = ['budget_category'], ax=ax, showfliers = False, positions = [2, 0, 1],
+               boxprops= dict(linewidth=2.5, color='blue'), 
+               whiskerprops=dict(linestyle='-',linewidth=2.5, color='black'), 
+               medianprops = dict(linewidth = 2.5, color = 'red'), capprops = dict(linewidth = 2.5))
+    
+    #Show horizontal grid lines
+    plt.grid(which='major', axis='x')
+    
+    #Create axis labels and a title and shift x-axis label down
+    ax.set_ylabel('Return on Investment')
+    ax.set_xlabel('Budget Tier')
+    ax.set_title('Distribution of Return on Investment Percentage Grouped by Budget Tier')
+    ax.xaxis.labelpad = 20
+    fig.suptitle("")
+        
+    #Format axis labels to contain percent signs and change labels on x-axis tick marks
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    plt.xticks([0, 1, 2,], ['Low', 'Mid', 'High'])
+    
+    #Show plot
+    return plt.show()
+
+
+def create_genre_dfs (DataFrame)
+
+    action = DataFrame[DataFrame['genres_Action_id'] == 1]
+    adventure = DataFrame[DataFrame['genres_Adventure_id'] == 1]
+    comedy = DataFrame[DataFrame['genres_Comedy_id'] == 1]
+    family = DataFrame[DataFrame['genres_Family_id'] == 1]
+    thriller = DataFrame[DataFrame['genres_Thriller_id'] == 1]
+    documentary = DataFrame[DataFrame['genres_Documentary_id'] == 1]
+    other = DataFrame[DataFrame['genres_tuple'] == '(0, 0, 0, 0, 0, 0, 0)']
+    drama = DataFrame[DataFrame['genres_Drama_id'] == 1]
+
+
+def genre_boxplot (column):
+
+    #Create a figure
+    fig,  ax = plt.subplots(figsize = (20,12))
+    
+    #Create a boxplot for all genres for a specific column, in this case column is ROI. Do now show outliers, increase the line thickness for all parts of the boxplot, and set the colors for the lines. 
+    ax.boxplot([adventure[column], 
+                thriller[column], 
+                comedy[column], 
+                action[column], 
+                family[column], 
+                drama[column], 
+                documentary[column], 
+                other[column]], 
+                showfliers = False, 
+                boxprops= dict(linewidth=2.0, color='blue'), 
+                whiskerprops=dict(linestyle='-',linewidth=2.0, color='black'), 
+                medianprops = dict(linewidth = 2.0, color = 'red'), capprops = dict(linewidth = 2.0))
+
+    #Create axis labels and a title and shift x-axis label down
+    ax.set_title('Distribution of Returns on Investment for Movie Genres for all Budget Ranges')
+    ax.set_ylabel('Return on Investment')
+    ax.set_xlabel('Movie Genre') 
+    ax.xaxis.labelpad = 20
+    fig.suptitle("")   
+    
+    #Format axis labels to contain percent signs, change labels on x-axis tick marks, and rotate the labels
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    plt.xticks([1, 2, 3, 4, 5, 6, 7, 8], ['Adventure', 'Thriller', 'Comedy', 'Action', 'Family', 'Drama', 'Documentary', 'Other'])
+    plt.xticks(rotation=25)
+    
+    #Show horizontal grid lines
+    plt.grid(which='major', axis='x')
+
+    #Show plot
+    return plt.show()
